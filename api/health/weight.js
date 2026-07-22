ad5b3a847a82c2b2e1105a1c657287ab652e60da
madebymichaelarae <michaelaraeg@gmail.com>
2026-07-22 11:40:43 -0400

    Update weight.js

--- api/health/weight.js
+++ api/health/weight.js
@@ -18,6 +18,10 @@ export default async function handler(req, res) {
       ]
     });
 
+    return res.status(200).json({
+  count: pages.length
+});
+
     const weights = pages
       .filter((page) => {
         const categoryName =
