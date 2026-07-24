f2501b7a2462cdf0309b85ec02366ee573954962
madebymichaelarae <michaelaraeg@gmail.com>
2026-07-24 16:40:39 -0400

    Update reading-widget.js

--- js/reading-widget.js
+++ js/reading-widget.js
@@ -1,6 +1,6 @@
 
-const currentEndpoint = "/api/reading/current";
-const statsEndpoint = "/api/reading/stats";
+const currentEndpoint = "/api/reading?view=current";
+const statsEndpoint = "/api/reading?view=stats";
 
 const elements = {
     loading: document.getElementById("reading-loading"),
